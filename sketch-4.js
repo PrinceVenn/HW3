d0905edf51668d391c2267533691c130622375ef
Koda Bryant <31840899+PrinceVenn@users.noreply.github.com>
2017-09-24 21:11:24 -0700

    Create sketch-4.js

---
+++ sketch-4.js
@@ -0,0 +1,52 @@
+
+colorMode(HSB);
+  noFill();
+  stroke(0,255,255);
+var h = 100;
+var u = 100;
+
+while(true) {
+  background(255);
+  colorMode(HSB);
+  noFill();
+  ellipse(h, 100, 30, 30);
+  h = h + 10;
+  
+  if (h >= 200) {
+ 
+while(true) {
+  background(255);
+  colorMode(HSB);
+  noFill();
+  ellipse(h, 100, 30, 30);
+ h = h - 10;
+      
+  if (h <= 100) {
+    
+while(true) {
+  background(255);
+  colorMode(HSB);
+  noFill();
+    
+    ellipse(h, u, 30, 30);
+      u = u - 10;
+  
+  if (u <= 10 )
+    
+while(true) {
+  background(255);
+  colorMode(HSB);
+  noFill();
+    
+    ellipse(h, u, 30, 30);
+      u = u + 10;
+  
+  if (u >= 100) {
+    
+while(true) {
+     background(255);
+  ellipse(h, u, 30, 30);
+    
+  
+						
+}}}}}}}}
