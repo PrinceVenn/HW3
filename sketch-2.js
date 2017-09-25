8bae95fc7b93ccc1dc843a279274eee1e4ea3874
Koda Bryant <31840899+PrinceVenn@users.noreply.github.com>
2017-09-24 20:31:34 -0700

    Create sketch-2.js

---
+++ sketch-2.js
@@ -0,0 +1,28 @@
+ colorMode(HSB);
+  noFill();
+  stroke(0,255,255);
+
+
+ellipse(100,100,30,30)
+background(255);
+ellipse(110,100,30,30)
+background(255);
+ellipse(120,100,30,30)
+background(255);
+ellipse(130,100,30,30)
+background(255);
+ellipse(140,100,30,30)
+background(255);
+ellipse(150,100,30,30)
+background(255);
+ellipse(160,100,30,30)
+background(255);
+ellipse(170,100,30,30)
+background(255);
+ellipse(180,100,30,30)
+background(255);
+ellipse(190,100,30,30)
+background(255);
+ellipse(200,100,30,30)
+background(255);
+ellipse(210,100,30,30)
